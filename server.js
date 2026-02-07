ff6106428850f7330c24728745f1954fad53adff
Preawpan Siriphalangkanont <87581600+rungpreawpan@users.noreply.github.com>
2026-02-07 16:15:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
 const app = require('./src/app');
 
 require('dotenv').config();
@@ -9,23 +8,6 @@ const PORT = 3000;
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-  },
-});
-
-// socket
-io.on('connection', (socket) => {
-  console.log('Client connected via Socket.IO');
-
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
-  });
-});
-
-app.set('io', io);
 server.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
